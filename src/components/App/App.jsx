5eb35e8fbf12ef83270436efecdab9f681fbd259
serge-belkin <cer_fi@yahoo.com>
2022-12-23 15:50:05 +0200

    add components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import css from './App.module.css';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [
+    
+  ],
+  name: ''
+}
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import css from './ContactForm.module.css';
+
+
+
+export default ContactForm;
