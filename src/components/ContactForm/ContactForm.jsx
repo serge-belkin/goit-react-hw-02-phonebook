543a68472bb2f57df661c5e55ef881f3a7091aff
serge-belkin <cer_fi@yahoo.com>
2022-12-26 09:21:39 +0200

    1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import css from './ContactForm.module.css';
 
-
+class ContactForm extends Component {
+  
+}
 
 export default ContactForm;
