27bf5735cf91a6931ac9edfd3a9bd40056897c2d
serge-belkin <cer_fi@yahoo.com>
2022-12-26 23:33:04 +0200

    fix deploy 9

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 
 import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
+import Filter from 'components/Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
